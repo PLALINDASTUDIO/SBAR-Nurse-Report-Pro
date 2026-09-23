d800a5868d21924d36bae8e738aa9c793d7fffeb
PLALINDASTUDIO <achraftnainchi12@gmail.com>
2026-09-23 16:06:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE = 'sbar-nurse-report-pro-v20260923-3';
+const APP_PREFIX = 'sbar-nurse-report-';
+const CORE = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE && key.startsWith(APP_PREFIX))
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const req = event.request;
+
+  // Navigations always prefer the newest GitHub version first.
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req, { cache: 'no-store' })
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(req)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(req))
+  );
+});
